576657b8465fc5db5b7f93e15ca9936679bda61c
Sriram <madsriram@deloitte.com>
2023-03-16 16:19:44 +0530

    Initial commit

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ console.log('Hello World...!');
 
 //git clone https://github.com/madsriram/second-depo-demo.git
 //git .add
-//git commit -m "Commiting message"
+//git commit -m "Some Commiting message"
 //git push
